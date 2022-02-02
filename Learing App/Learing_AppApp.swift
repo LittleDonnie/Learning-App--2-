ce3b5c66e69c50c007d8ad28a34c858157c66630
Monty Montgomery <poweredbyplants@gmail.com>
2022-02-02 09:16:04 -0600

    Initial Commit

---
+++ Learing App/Learing_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learing_AppApp.swift
+//  Learing App
+//
+//  Created by Monty Montgomery on 2/2/22.
+//
+
+import SwiftUI
+
+@main
+struct Learing_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
